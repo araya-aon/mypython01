f8fff1b8032412d500abd3fb75d9420eea7a897d
araya-aon <Araya.aon.m@gmail.com>
2025-08-15 11:46:57 +0700

    V.2: create file 02-05

---
+++ test02.py
@@ -0,0 +1,11 @@
+# statement คำสั่ง เขียนอยู่ 2 ลักษณะ
+#  - 1 บรรทัด 1 คำสั่ง จะปิดท้ายด้วย ; หรือไม่ก็ได้
+print('Hello')
+print('Hi man')
+
+# - 1 บรรทัด หลายคำสั่ง จะแยกแต่ละคำสั่งด้วย ; ยกเว้นคำสั่งชุดสุดท้าย
+print('AAA'); print('BBB'); print('CCC')
+print('DDD'); print('EEE'); print('FFF')
+
+# ----------------------
+print('holy water is so good')
